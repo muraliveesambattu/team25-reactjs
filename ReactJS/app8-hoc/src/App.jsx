47f06d4b15579d3c6f75af037cb0e0b75940f0b6
mveesambattu <mveesambattu@uynite.com>
2025-03-24 10:58:27 +0530

    Higher Order Components

---
+++ ReactJS/app8-hoc/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ClickCounterDemo from "./Components/ClickCounterDemo";
+import HoverCounterDemo from "./Components/HoverCounterDemo";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <ClickCounterDemo />
+      <hr />
+      <HoverCounterDemo />
+    </>
+  );
+}
+
+export default App;
