4e5f486f9c89e86da883b5fb544870428897fb66
Murali Veesambattu <murali.veesambattu@gmail.com>
2025-04-19 15:01:31 +0530

    info

---
+++ ReactJS/app17-redux-typescript-demo/src/Components/User.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface UserInterface {
+    hello: string;
+    info:string
+}
+const User = (props: UserInterface) => {
+    const changeValue = (num1: number, num2: number) => {
+        console.log(num1 + num2)
+    };
+
+    return (
+        <div>
+            <h2>Welcome to User Component !!!</h2>
+            <button onClick={() => { changeValue(12, 13) }}>Change Value </button>
+            <h2>{props.hello}</h2>
+        </div>
+    )
+}
+
+export default User
